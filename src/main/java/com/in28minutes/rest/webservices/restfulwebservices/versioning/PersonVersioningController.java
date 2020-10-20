5a274da0861e54658895624f69a20cf887473d93
vikas0693 <vikas06sharma93@gmail.com>
2020-10-20 22:47:17 +0530

    Different ways of versioning added.Prams,headers and produces are 3 different ways,

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -14,4 +14,30 @@ public class PersonVersioningController {
 	public PersonV2 personV2() {
 		return new PersonV2(new Name("Bob","Charlie"));
 	}
+	@GetMapping(value="/person/param",params="version=1")
+	public PersonV1 paramV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	@GetMapping(value="/person/param",params="version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Bob","Charlie"));
+	}
+	@GetMapping(value="/person/param",headers="X-API-VERSION=1")
+	public PersonV1 headerV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	@GetMapping(value="/person/param",headers="X-API-VERSION=2")
+	public PersonV2 headerV2() {
+		return new PersonV2(new Name("Bob","Charlie"));
+	}
+	//pass produces value in Accept headder of request
+	@GetMapping(value="/person/produces",produces="application/vnd.company.app-v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	//pass produces value in Accept headder of request
+	@GetMapping(value="/person/produces",produces="application/vnd.company.app-v2+json")
+	public PersonV2 producesV2() {
+		return new PersonV2(new Name("Bob","Charlie"));
+	}
 }
